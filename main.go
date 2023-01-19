5e7f06aa03f75dba299c027fbd8f23bf31a831b6
RealLanta <95084339+RealLanta@users.noreply.github.com>
2023-01-20 01:22:35 +0800

    Update main.go

--- main.go
+++ main.go
@@ -9,11 +9,11 @@ import (
 	"runtime"
 	"syscall"
 
-	"github.com/Dreamacro/clash/config"
-	C "github.com/Dreamacro/clash/constant"
-	"github.com/Dreamacro/clash/hub"
-	"github.com/Dreamacro/clash/hub/executor"
-	"github.com/Dreamacro/clash/log"
+	"github.com/RealLanta/clash/config"
+	C "github.com/RealLanta/clash/constant"
+	"github.com/RealLanta/clash/hub"
+	"github.com/RealLanta/clash/hub/executor"
+	"github.com/RealLanta/clash/log"
 
 	"go.uber.org/automaxprocs/maxprocs"
 )
